cabd64e03150b2fe26fc2f5dd61923586caebac5
Bilal El Houdaigui <bilal@ebi.ac.uk>
2020-12-10 10:27:02 +0000

    EA-653 replaced the ugly part with an end of list check

--- trackhubs/parser.py
+++ trackhubs/parser.py
@@ -38,43 +38,42 @@ def parse_file_from_url(url, is_hub=False, is_genome=False, is_trackdb=False):
     # e.g. dict_info = {'track': 'JASPAR2020_TFBS_hg19', 'type': 'bigBed 6 +'}
     dict_info = {}
     dict_info_list = []
+
     try:
-        for line in urllib.request.urlopen(url):
-            line = line.decode("utf-8").strip()
-            # new line marks a new genome/track/supertrack etc
-            # in this case we check if dict_info is not empty
-            # then we append it to dict_info_list and empty the
-            # dictionary fot the next element otherwise (if there is no new line)
-            # we read the next line and we repeat the process
-            if line in ['\n', '\r\n', '']:
-                # check if the dictionary contains either 'hub',
-                # 'track', 'genome' before appending it to the list
-                # this prevent the submitter from uploading random text file
-                is_any = any(i in list(dict_info.keys()) for i in ('hub', 'track', 'genome'))
-                if len(dict_info) > 0 and is_any:
-                    dict_info.update({'url': url})
-                    dict_info_list.append(dict_info)
-                    dict_info = {}
-                continue
+        with urllib.request.urlopen(url) as f:
+            # read the whole file into a list
+            # each element represent one line from the file
+            file_content = f.read().decode('utf-8').splitlines()
+            # the length is used to check if we reached the end of the file
+            len_file_content = len(file_content)
+
+        for line_number, line in enumerate(file_content):
+            if line not in ['\n', '\r\n', ''] and not line.startswith('#'):
+                split_line = line.rstrip('\n').split(' ', 1)
+                key = split_line[0].strip()
+                dict_info[key] = split_line[1]
 
             # there are tracks with hashtag symbol! => ignore them!
             # e.g http://ftp.ebi.ac.uk/pub/databases/ensembl/encode/integration_data_jan2011/hg19/trackDb.txt
             if line.startswith('#'):
                 continue
 
-            split_line = line.rstrip('\n').split(' ', 1)
-            key = split_line[0].strip()
-            dict_info[key] = split_line[1]
-
-        # This part is ugly, but it's the only way I found to make sure the last
-        # element is added to the returned dict_info_list (in case there is no new line
-        # at the end of the parsed file)
-        # TODO: find a better way to detect the EOF
-        if (is_hub and 'hub' in dict_info and not any(d.get('hub') == dict_info.get('hub') for d in dict_info_list)) or \
-                (is_genome and 'genome' in dict_info and not any(d.get('genome') == dict_info.get('genome') for d in dict_info_list)) or \
-                (is_trackdb and 'track' in dict_info and not any(d.get('track') == dict_info.get('track') for d in dict_info_list)):
-            dict_info.update({'url': url})
-            dict_info_list.append(dict_info)
+            # new line marks a new genome/track/supertrack etc
+            # in this case we check if dict_info is not empty
+            # then we append it to dict_info_list and empty the
+            # dictionary for the next element otherwise (if there is no new line)
+            # we read the next line and we repeat the process
+            # also we add the last object when we reach the EOF
+            if line in ['\n', '\r\n', ''] or line_number+1 == len_file_content:
+                # check if the dictionary contains either 'hub',
+                # 'track' or 'genome' key before appending it to the list
+                # this prevents the submitter from uploading random text file
+                is_either_hub_track_or_genome = any(i in list(dict_info.keys()) for i in ('hub', 'track', 'genome'))
+                if len(dict_info) > 0 and is_either_hub_track_or_genome:
+                    dict_info.update({'url': url})
+                    dict_info_list.append(dict_info)
+                    dict_info = {}
+                continue
 
     except (IOError, urllib.error.HTTPError, urllib.error.URLError, ValueError, AttributeError, TypeError) as ex:
         logger.error(ex)
