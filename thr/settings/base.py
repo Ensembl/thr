dc343c931583ef53ddd43b1b311015e9c35f7203
Bilal El Houdaigui <bilal@ebi.ac.uk>
2021-06-04 12:55:03 +0100

    set item per page to 5 and add verbose mode to hubcheck

--- thr/settings/base.py
+++ thr/settings/base.py
@@ -73,7 +73,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 5,
     'ORDERING_PARAM': 'ordering',
 }
 
