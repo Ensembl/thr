Ensembl/thr

commit b3174b4b639c24e44f86ea700d8c41ffab629bb3

:ambulance: EA-814 replaced JSON field with LONGTEXT to support MySQL 6

trackhubs/models.py
@@ -22,10 +22,8 @@ from django_elasticsearch_dsl_drf.wrappers import dict_to_obj
 from django_mysql.models import JSONField
 import elasticsearch
 from elasticsearch_dsl import connections
-from django.contrib.auth import get_user_model
+from users.models import CustomUser as User
 import trackhubs
-
-User = get_user_model()
 
 logger = logging.getLogger(__name__)
 
@@ -54,7 +52,7 @@ class FileType(models.Model):
 
     file_type_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    settings = JSONField()
+    settings = models.TextField(null=True)  # JSONField()
 
 
 class Visibility(models.Model):
@@ -181,9 +179,9 @@ class Trackdb(models.Model):
     version = models.CharField(default="v1.0", max_length=10)
     created = models.IntegerField(default=int(time.time()))
     updated = models.IntegerField(null=True)
-    configuration = JSONField()
-    data = JSONField()
-    status = JSONField()
+    configuration = models.TextField(null=True)  # JSONField()
+    data = models.TextField(null=True)  # JSONField()
+    status = models.TextField(null=True)  # JSONField()
     source_url = models.CharField(max_length=255, null=True)
     source_checksum = models.CharField(max_length=255, null=True)
     assembly = models.ForeignKey(Assembly, on_delete=models.CASCADE)
@@ -327,7 +325,7 @@ class Track(models.Model):
     big_data_url = models.CharField(max_length=255, null=True)
     html = models.CharField(max_length=255, null=True)
     meta = models.CharField(max_length=255, null=True)
-    additional_properties = JSONField()
+    additional_properties = models.TextField(null=True)  # JSONField()
     composite_parent = models.CharField(max_length=2, null=True)
     parent = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
     trackdb = models.ForeignKey(Trackdb, on_delete=models.CASCADE)

commit 2372653a817b57517f6875ee29f7c617c6f052d0

:ambulance: EA-814 fixed broken API calls and ES enrich (caused by b3174b4)

trackdbs/serializers.py
@@ -12,6 +12,7 @@
    limitations under the License.
 """
 from datetime import datetime
+from trackhubs.utils import str2obj
 
 from rest_framework import serializers
 from trackhubs import models
@@ -71,6 +72,8 @@ class TrackdbSerializer(serializers.ModelSerializer):
     hub = serializers.SerializerMethodField()
     species = serializers.SerializerMethodField()
     assembly = serializers.SerializerMethodField()
+    configuration = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Trackdb
@@ -128,6 +131,14 @@ class TrackdbSerializer(serializers.ModelSerializer):
     def get_assembly(obj):
         assembly = models.Assembly.objects.filter(assembly_id=obj.assembly.assembly_id).first()
         return TrackdbAssemblySerializer(assembly).data
+
+    @staticmethod
+    def get_configuration(obj):
+        return str2obj(obj.configuration)
+
+    @staticmethod
+    def get_status(obj):
+        return str2obj(obj.status)
 
     @staticmethod
     def get_file_type(obj):

trackhubs/models.py
@@ -24,6 +24,7 @@ import elasticsearch
 from elasticsearch_dsl import connections
 from users.models import CustomUser as User
 import trackhubs
+from trackhubs.utils import str2obj
 
 logger = logging.getLogger(__name__)
 
@@ -52,7 +53,7 @@ class FileType(models.Model):
 
     file_type_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    settings = models.TextField(null=True)  # JSONField()
+    settings = models.TextField(null=True, default='')  # JSONField()
 
 
 class Visibility(models.Model):
@@ -102,7 +103,7 @@ class Hub(models.Model):
                     'schema': trackdb.version,
                     'created': datetime.utcfromtimestamp(trackdb.created).strftime('%Y-%m-%d %H:%M:%S'),
                     'updated': datetime.utcfromtimestamp(trackdb.updated).strftime('%Y-%m-%d %H:%M:%S'),
-                    'status': trackdb.status,
+                    'status': str2obj(trackdb.status),
                 }
             )
         return trackdbs_list
@@ -179,9 +180,9 @@ class Trackdb(models.Model):
     version = models.CharField(default="v1.0", max_length=10)
     created = models.IntegerField(default=int(time.time()))
     updated = models.IntegerField(null=True)
-    configuration = models.TextField(null=True)  # JSONField()
-    data = models.TextField(null=True)  # JSONField()
-    status = models.TextField(null=True)  # JSONField()
+    configuration = models.TextField(null=True, default='')  # JSONField()
+    data = models.TextField(null=True, default='')  # JSONField()
+    status = models.TextField(null=True, default='')  # JSONField()
     source_url = models.CharField(max_length=255, null=True)
     source_checksum = models.CharField(max_length=255, null=True)
     assembly = models.ForeignKey(Assembly, on_delete=models.CASCADE)
@@ -274,8 +275,9 @@ class Trackdb(models.Model):
         Update trackdb document in Elascticsearch with the additional data provided
         :param trackdb: trackdb object to be updated
         :param file_type: file type associated with this track
-        :param trackdb_data: data array that will be added to the trackdb document
-        :param trackdb_configuration: configuration object that will be added to the trackdb document
+        :param trackdb_data: data object that will be added to the trackdb document
+        :param trackdb_configuration: configuration string that will be added to the trackdb document
+        :param tracks_status: status dictionary that will be added to the trackdb document
         :param index: index name (default: 'trackhubs')
         :param doc_type: document type (default: 'doc')
         # TODO: handle exceptions
@@ -292,7 +294,7 @@ class Trackdb(models.Model):
                     'doc': {
                         'owner': hub.get_owner(),
                         'file_type': self.get_trackdb_file_type_count(),
-                        'data': trackdb_data,
+                        'data': str2obj(trackdb_data),
                         'updated': int(time.time()),
                         'source': {
                             'url': self.source_url,
@@ -302,8 +304,8 @@ class Trackdb(models.Model):
                         'type': trackhubs.models.Hub.objects.filter(data_type_id=hub.data_type_id)
                             .values('data_type__name').first()
                             .get('data_type__name'),
-                        'configuration': trackdb_configuration,
-                        'status': tracks_status
+                        'configuration': str2obj(trackdb_configuration),
+                        'status': str2obj(tracks_status)
                     }
                 }
             )